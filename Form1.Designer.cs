5c31f0a2ead9320a6ff21e1f1ff884b8f7d85395
RichardsRobinR <richardsrobin.r15@gmail.com>
2021-07-27 19:24:27 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Connected_Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rollNo_TextBox = new System.Windows.Forms.TextBox();
+            this.Branch_TextBox = new System.Windows.Forms.TextBox();
+            this.Name_TextBox = new System.Windows.Forms.TextBox();
+            this.create_Button = new System.Windows.Forms.Button();
+            this.Update_Button = new System.Windows.Forms.Button();
+            this.Delete_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rollNo_TextBox
+            // 
+            this.rollNo_TextBox.Location = new System.Drawing.Point(72, 41);
+            this.rollNo_TextBox.Name = "rollNo_TextBox";
+            this.rollNo_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.rollNo_TextBox.TabIndex = 0;
+            // 
+            // Branch_TextBox
+            // 
+            this.Branch_TextBox.Location = new System.Drawing.Point(72, 173);
+            this.Branch_TextBox.Name = "Branch_TextBox";
+            this.Branch_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.Branch_TextBox.TabIndex = 1;
+            // 
+            // Name_TextBox
+            // 
+            this.Name_TextBox.Location = new System.Drawing.Point(72, 97);
+            this.Name_TextBox.Name = "Name_TextBox";
+            this.Name_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.Name_TextBox.TabIndex = 2;
+            // 
+            // create_Button
+            // 
+            this.create_Button.Location = new System.Drawing.Point(72, 237);
+            this.create_Button.Name = "create_Button";
+            this.create_Button.Size = new System.Drawing.Size(100, 36);
+            this.create_Button.TabIndex = 3;
+            this.create_Button.Text = "Create";
+            this.create_Button.UseVisualStyleBackColor = true;
+            this.create_Button.Click += new System.EventHandler(this.create_Button_Click);
+            // 
+            // Update_Button
+            // 
+            this.Update_Button.Location = new System.Drawing.Point(204, 237);
+            this.Update_Button.Name = "Update_Button";
+            this.Update_Button.Size = new System.Drawing.Size(100, 36);
+            this.Update_Button.TabIndex = 4;
+            this.Update_Button.Text = "Update";
+            this.Update_Button.UseVisualStyleBackColor = true;
+            this.Update_Button.Click += new System.EventHandler(this.Update_Button_Click);
+            // 
+            // Delete_Button
+            // 
+            this.Delete_Button.Location = new System.Drawing.Point(340, 237);
+            this.Delete_Button.Name = "Delete_Button";
+            this.Delete_Button.Size = new System.Drawing.Size(100, 36);
+            this.Delete_Button.TabIndex = 5;
+            this.Delete_Button.Text = "Delete";
+            this.Delete_Button.UseVisualStyleBackColor = true;
+            this.Delete_Button.Click += new System.EventHandler(this.Delete_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Delete_Button);
+            this.Controls.Add(this.Update_Button);
+            this.Controls.Add(this.create_Button);
+            this.Controls.Add(this.Name_TextBox);
+            this.Controls.Add(this.Branch_TextBox);
+            this.Controls.Add(this.rollNo_TextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox rollNo_TextBox;
+        private System.Windows.Forms.TextBox Branch_TextBox;
+        private System.Windows.Forms.TextBox Name_TextBox;
+        private System.Windows.Forms.Button create_Button;
+        private System.Windows.Forms.Button Update_Button;
+        private System.Windows.Forms.Button Delete_Button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connected_Database
+{
+
+    public partial class Form1 : Form
+    {
+
+        String connectionstring;
+        SqlConnection sqlConnection;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectionstring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\richa\Documents\WindowsForms\Connected_Database\Database1.mdf;Integrated Security=True";
+
+            sqlConnection = new SqlConnection(connectionstring);
+
+            sqlConnection.Open();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            sqlConnection.Close();
+        }
+
+        private void create_Button_Click(object sender, EventArgs e)
+        {
+            SqlParameter parameter_1 = new SqlParameter();
+            parameter_1.ParameterName = "rollno_p";
+            parameter_1.Value = int.Parse(rollNo_TextBox.Text);
+
+            SqlParameter parameter_2 = new SqlParameter();
+            parameter_2.ParameterName = "name_p";
+            parameter_2.Value = Name_TextBox.Text;
+
+            SqlParameter parameter_3 = new SqlParameter();
+            parameter_3.ParameterName = "branch_p";
+            parameter_3.Value = Branch_TextBox.Text;
+
+
+            SqlCommand sqlCommand = new SqlCommand("INSERT INTO student VALUES(@rollno_p,@name_p,@branch_p)");
+            sqlCommand.Connection = sqlConnection;
+
+
+            sqlCommand.Parameters.Add(parameter_1);
+            sqlCommand.Parameters.Add(parameter_2);
+            sqlCommand.Parameters.Add(parameter_3);
+
+            sqlCommand.ExecuteNonQuery();
+
+
+        }
+
+        private void Update_Button_Click(object sender, EventArgs e)
+        {
+            SqlParameter parameter_1 = new SqlParameter();
+            parameter_1.ParameterName = "rollno_p";
+            parameter_1.Value = int.Parse(rollNo_TextBox.Text);
+
+            SqlParameter parameter_2 = new SqlParameter();
+            parameter_2.ParameterName = "name_p";
+            parameter_2.Value = Name_TextBox.Text;
+
+            SqlParameter parameter_3 = new SqlParameter();
+            parameter_3.ParameterName = "branch_p";
+            parameter_3.Value = Branch_TextBox.Text;
+
+            SqlCommand sqlCommand = new SqlCommand("UPDATE student Set name = @name_p,branch = @branch_p WHERE rollno = @rollno_p");
+            sqlCommand.Connection = sqlConnection;
+
+
+            sqlCommand.Parameters.Add(parameter_1);
+            sqlCommand.Parameters.Add(parameter_2);
+            sqlCommand.Parameters.Add(parameter_3);
+
+            sqlCommand.ExecuteNonQuery();
+        }
+
+        private void Delete_Button_Click(object sender, EventArgs e)
+        {
+            SqlParameter parameter_1 = new SqlParameter();
+            parameter_1.ParameterName = "rollno_p";
+            parameter_1.Value = int.Parse(rollNo_TextBox.Text);
+
+            SqlCommand sqlCommand = new SqlCommand("DELETE FRom student WHERE rollno = @rollno_p");
+            sqlCommand.Connection = sqlConnection;
+
+            sqlCommand.Parameters.Add(parameter_1);
+
+            sqlCommand.ExecuteNonQuery();
+        }
+    }
+}
